af6e9117ee3a5a032d92055221e93afd8bb6deff
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-02-23 15:39:11 +0900

    Removed input tests -- defer to Playwright 0.10

--- cypress/support/getCodex.js
+++ cypress/support/getCodex.js
@@ -1,10 +1,10 @@
-const NO_DELAY = { delay: 0 }
-
-Cypress.Commands.add("getCodex", {
-	prevSubject: "element",
-}, (subject, data, options = NO_DELAY) => {
-	cy.wrap(subject, options)
-		.window(options)
-		.then(window => window.getCodex(), options)
-		.should("eq", data, options)
-})
+// const NO_DELAY = { delay: 0 }
+//
+// Cypress.Commands.add("getCodex", {
+// 	prevSubject: "element",
+// }, (subject, data, options = NO_DELAY) => {
+// 	cy.wrap(subject, options)
+// 		.window(options)
+// 		.then(window => window.getCodex(), options)
+// 		.should("eq", data, options)
+// })
